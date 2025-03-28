d77b47ac1d25f225d2bdaa58b89d52e8f41c168b
excell <3061266318@qq.com>
2025-03-28 09:51:16 +0800

    配置信息修改完成

--- sky-common/src/main/java/com/sky/properties/RedisProperties.java
+++ sky-common/src/main/java/com/sky/properties/RedisProperties.java
@@ -5,15 +5,24 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 /**
- * @description: redis配置参数
+ * @description: Redis的配置信息
  * @author: Excell
- * @data 2025年03月24日 21:55
+ * @data 2025年03月28日 09:39
  */
+@Data
 @Component
 @ConfigurationProperties(prefix = "sky.redis")
-@Data
 public class RedisProperties {
     private String host;
     private Integer port;
     private String password;
+    private LettucePool lettucePool = new LettucePool();
+
+    @Data
+    public static class LettucePool {
+        private Integer maxActive;
+        private Integer maxIdle;
+        private Integer minIdle;
+        private String timeBetweenEvictionRuns;
+    }
 }
